1c1925e3922e6309d81a62aa130dca97ede4a764
ChrisPan <szupzj18@outlook.com>
2025-05-29 02:36:42 +0800

    update syntax define

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import parser from "./tsc";
-import ICore from "./core";
-
-export default function main(params: string) {
-  console.log(`hello ${params} tsc.`);
-  parser(params);
-}
-
-main("world");
--- src/mini-tsc.ts
+++ src/mini-tsc.ts
@@ -26,3 +26,23 @@ interface Token {
   line: number;
   column: number;
 }
+
+enum SyntaxKind {
+  SourceFile, // Represents the entire source file
+  VariableDeclaration, // Represents a variable declaration
+  Identifier, // Represents an identifier (variable name)
+  NumberLiteral, // Represents a numeric literal
+  StringLiteral, // Represents a string literal
+  NumberKeywordToken, // Represents the 'number' keyword
+  StringKeywordToken, // Represents the 'string' keyword
+}
+
+interface Node {
+  kind: SyntaxKind;
+  pos: number; // Start position in the source code
+  end: number; // End position in the source code
+}
+
+interface SourceFile extends Node {}
+
+interface Statement extends Node {}
